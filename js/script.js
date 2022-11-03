cc202743c02f20c63a448ab4c88029b237cc0375
mksainn <116638287+mksainn@users.noreply.github.com>
2022-11-03 10:39:22 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","web Developer","Graphic Designer","Youtuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
